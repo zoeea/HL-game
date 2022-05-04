b5afea1e74269c0bfe64a7ff19d0e0a545c9482a
zoeea <99296464+zoeea@users.noreply.github.com>
2022-05-05 10:44:10 +1200

    working now. next is game stats.

--- 01_HLG_Base_component_v2.py
+++ 01_HLG_Base_component_v2.py
@@ -1,5 +1,7 @@
-# Function used to check input is valid
+import math
+import random
 
+# Function used to check input is valid
 
 # Check for an integer more than 0
 def num_check(question):
@@ -25,13 +27,152 @@ def num_check(question):
                 continue
 
         return response
+def check_rounds():
+    while True:
+
+        round_error = "Please type either <enter> " \
+                      "or an integer that is more than 0\n "
+
+        # if infinite mode not chosen, check response
+        # is an integer that is more than 0
+        if response != "":
+            try:
+                response = int(response)
+
+                # if response is too low, go bak to
+                # start of loop
+                if response < 1:
+                    print(round_error)
+                    continue
+
+            # if response is not an integer go back to
+            # start of loop
+            except ValueError:
+                print(round_error)
+                continue
+        
+        return response
+
+def choice_checker(question, valid_list, error):
+
+    valid = False
+    while not valid:
+
+        # Ask user for choice (and put choice in lowercase)
+        response = input(question).lower()
+
+        # iterates through list and if response is an item
+        # in the list (or the first letter of an item), the
+        # full item name is returned
+
+        for item in valid_list:
+            if response == item[0] or response == item:
+                return item
+
+        # output error if item not in list
+        print(error)
+        print()
+
+def intcheck(question, low=None, high=None, exit_code = None):
+
+    while True:
+
+        # sets up error messages
+        if low is not None and high is not None:
+            error = "Please enter an integer between {} and {} (inclusive)".format(low, high)
+        elif low is not None and high is None:
+            error = "Please enter an integer that is more than or equal to {}".format(low)
+        elif low is None and high is not None:
+            error = "Please enter an integer that is less than or equal to {}".format(high)
+        else:
+            error = "Please enter an integer"
+
+        try:
+            response = input(question)
+            
+            # check to see if response is the exit code and return it
+            if response == exit_code:
+                return response
+            
+            # change the response into an integer
+            else:
+                response = int(response)
+
+            # Checks response is not too low, not use of 'is not' keywords
+            if low is not None and response < low:
+                print(error)
+                continue
+
+            # Checks response is not too high
+            if high is not None and response > high:
+                print(error)
+                continue
+
+            return response
+
+        # checks input is a integer
+        except ValueError:
+            print(error)
+            continue
+
+# outputs instructions, returns ""
+def instructions():
+    print(""" 
+        *** Instructions ****
+        *****To begin with the computer will ask you to choose a number of rounds OR press <enter> for infinite mode****
+        *****Next you will guess a number , once selecting what numbers you will be guessing between(high and low number)***
+        *****If your number is too high the computer will tell you to go lower , and visa versa, until you guess the correct number****
+          """)
+
+    return ""
 
 # Main routine goes here....
 
 rounds_played = 0
 
+# intialise game parameters etc
+rounds_played = 0
+
+
+
+# welcome user
+print()
+print("**** Welcome to the great higher or lower Game *****")
+print()
+
+# ask user if they need instructions...
+played_before = choice_checker("Have you played the game before? ", ["yes", "no"], "Please enter yes or no")
+print()
+
+if played_before == "no":
+    instructions()
+
+# get game parameters (loc, high)
+rounds = 3      # replace with question asking how many rounds (etc)
+
+# ask for low / high number
 # Ask user for # of rounds, <enter> for infinite mode
 rounds = num_check("How many rounds? <enter> for infinite mode")
+
+if rounds == "":
+    print("you chose infinite mode")
+else:
+    print("you asked for {} rounds".format(rounds))
+
+# checks that response is an integer    
+low_num = intcheck("Low Number: ")
+print("You chose a low number of ", low_num)
+
+# checks that response is an integer more than the low number
+high_num = intcheck("High Number: ", low_num)
+print("You chose a high number of ", high_num)
+
+# works out max guesses based on user selected range
+range = high_num - low_num + 1
+max_raw = math.log2(range)  #finds maximum # of guesses using
+max_upped = math.ceil(max_raw)  #rounds up ( ceil -> ceiling)
+max_guesses = max_upped + 1
+print("Max Guesses: {}".format(max_guesses))
 
 if rounds == "":
     mode = "infinite"
@@ -55,7 +196,8 @@ while end_game == "no":
 
     # Rounds start here
 
-    secret = 7  # replace with secret number in due course
+    secret = random.randint(low_num, high_num)
+    print("Spoiler alert", secret)
 
     # Rounds Heading
     print()
@@ -73,28 +215,23 @@ while end_game == "no":
 
 
     guess = ""
-    # guessing for the round goes here
     while guess != secret:
-    #  Guessing starts here...
+        guess = int(input("Guess: "))  # replace with number checker in due course
 
-        guess = num_check("Guess")  # replace with call to number checking function
-        
         # end game if exit code is typed
         if guess == "xxx":
-            end_game = "yes"
             break
-            
-        # elif rounds_played == rounds:
-        #     end_game = "yes"
-        #     break
+       
 
-        # check user guess
+        if guess < secret:
+            print("guess higher")
+        elif guess > secret:
+            print("guess lower")
+        else:
+            print("You have guessed the correct number! ")
+            break
 
 
-        # rest of loop / game
-        print("You guessed: {}".format(guess))
-
-    # end game if required
 
 print("Thank you for playing")
 
