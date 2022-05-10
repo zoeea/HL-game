27a1a657446d40693c1982e8dd370e1ac2f82a0b
zoeea <99296464+zoeea@users.noreply.github.com>
2022-05-10 13:06:36 +1200

    working the game stats out

---
+++ 10_stats_trials_2.py
@@ -0,0 +1,14 @@
+
+# find the lowest number
+
+#declaring a list
+list0 = [4, 10, 5, 4, 1, 6]
+print ("list = ", list0)
+ 
+#finding smallest number
+s_num = min(list0)
+s_num_max = max(list0)
+average = sum(list0) / len(list0)
+print ("The smallest number of gueeses in any given round was: ", s_num)
+print ("The largest number of gueeses in any given round was: ", s_num_max)
+print("Average: {}".format(average))
